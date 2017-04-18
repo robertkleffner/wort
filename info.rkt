970bf4ebab3ccab39108c7438e58c8c70fdc0df7
Robert Kleffner <gamecodeshark@gmail.com>
2017-04-18 18:48:53 -0400

    hopefully got all the dependencies right now.

--- info.rkt
+++ info.rkt
@@ -3,7 +3,7 @@
 (define deps '("base"
                "rackunit-lib"
                "brag"
-               "br"))
+               "beautiful-racket"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/wort.scrbl" ())))
 (define pkg-desc "A core concatenative language with type inference")
